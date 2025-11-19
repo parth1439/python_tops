8ea4cf3893b55c784e16f4e0cdf68816479cdc5d
parth <parthkacha@05gmail.com>
2025-11-19 07:36:56 +0530

    all

---
+++ Revision.py
@@ -0,0 +1,3 @@
+a=37
+b=5
+print("remainder a is divisible by b:",a%b)
--- basic.py
+++ basic.py
@@ -1,1 +1,125 @@
-print("hello"end=." ")
+#a = int(int(input( "1 number is :"))
+#b =) int(int(input( "2 number is :"))
+
+
+#p)rint("number a is :",a)
+#print("number b is :",b)
+#print("sum is ",a+b)
+
+
+#add to number
+#a=3
+#b=4
+#print(a+b)
+
+#a=37 
+#b=5
+#print("remainder a is divisible by b:",a%b)
+
+
+#a greather than b
+#a= int(int(input("enter a number 1:"))
+#)b= int(int(input("enter a number 2:"))
+
+#print("a is greather than b:",a>b)
+
+# average find of 2 number
+#a =int(int(input("enter a number 1:"))
+#)b =int(int(input("enter a number 2:"))
+
+#print("the average of these two numbers:",(a+b)/2)
+
+#square of two number
+#a=int(int(input("enter your number:"))
+
+#print("square of two numbber:",a*a)
+
+
+""" here = "parth"
+nameshort = here[0:2]
+print(nameshort)
+charcter = here[1]
+print(charcter) """
+
+#negative indices
+""" here = "parth"
+print(here[-4:-1])
+print(here[1:4]) """
+
+
+#string function
+""" here = "parth"
+print(here.endswith("rth")) """
+
+
+#escape sequence character
+""" a="parth is a good boy but not a bad \"boy\""
+print(a) """
+
+#fstring use
+""" here= int(input("enter your here:")
+pri)nt(f"good afternoon {here}") """
+
+""" letter = '''dear <|here|>,
+you are selected!
+<|date|>,
+<|surname|>'''
+
+print(letter.replace("<|here|>","parth").replace("<|date|>","15 novmenber 2025").replace("<|surname|>","kacha"))
+ """
+
+
+""" #find double space
+here= "parth is a good  boy"
+print(here.replace("  "," ")) """
+
+#list
+""" friends=["apple","orange",5,345.56,False,"parth"]
+print(friends[0])
+friends[0]="grapes"
+print(friends[0])
+
+#list append
+friends.append("harry")
+print(friends)"""
+
+#list sort
+"""l1=[1,47,87,39,4]
+l1.sort()
+print(l1) 
+#list insert
+l1.insert(3,45)
+print(l1) 
+l1.remove()
+print(l1)"""
+
+#tuple
+""" a= (12,"parth",False,334,45,"rohan")
+i=a.index("12")
+print(i) """
+
+#int(input 7 frutis here
+
+""" marks=[]
+f1=int(input("enter a marks here :"))
+marks.append(f1)
+f2=int(input("enter a marks here :"))
+marks.append(f2)
+f3=int(input("enter a marks here :"))
+marks.append(f3)
+f4=int(input("enter a marks here :"))
+marks.append(f4)
+f5=int(input("enter a marks here :"))
+marks.append(f5)
+f6=int(input("enter a marks here :"))
+marks.append(f6)
+
+marks.sort()
+print(marks) """
+
+""" l=[3,4,3,2]
+print(sum(l)) """
+
+a=[7,0,8,0,0,9]
+n=a.count(0)
+print(n)
